98ff716181e9df1f0f879de76c6f97719f1b6a22
BFElliot <bourdetfumardelliot@gmail.com>
2026-05-27 13:49:47 +0200

    add parsing

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dminh <dminh@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ebourdet <ebourdet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 13:47:31 by dminh             #+#    #+#             */
-/*   Updated: 2026/05/21 15:14:58 by dminh            ###   ########.fr       */
+/*   Updated: 2026/05/27 13:49:04 by ebourdet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,29 +24,45 @@
 # include "mlx.h"
 # include "libft.h"
 
-# define TILES
-
-typedef struct s_player
-{
-	float	pos_x;
-	float	pos_y;
-	float	dir_x;
-	float	dir_y;
-	float	plane_x;
-	float	plane_y;
-}	t_player;
-
 typedef struct s_map
 {
-
+	char	**grid;
+	int		width;
+	int		height;
+	char	*no_tex_path;
+	char	*so_tex_path;
+	char	*we_tex_path;
+	char	*ea_tex_path;
+	int		floor_color;
+	int		ceiling_color;
+	t_list	*raw_map;
+	int		map_started;
+	int		empty_line_seen;
 }	t_map;
 
-typedef struct s_game
-{
-	void		*mlx_ptr;
-	void		*win_ptr;
-	t_player	*player;
-	t_map		*map;
-}	t_game;
+void	init_map_struct(t_map *map);
+int		check_extension(char *filename, char *ext);
+void	skip_spaces(char *line, int *i);
+int		route_line(char *line, t_map *map);
+int		is_texture(char *str);
+int		is_color(char *str);
+int		is_empty_or_whitespace(char *str);
+int		process_lines(int fd, t_map *map);
+char	*get_valid_path(char *str);
+int		parse_texture(char *line, t_map *map);
+int		parse_cub(char *filename, t_map *map);
+int		parse_rgb(char **rgb);
+int		parse_color(char *line, t_map *map);
+int		all_elements_loaded(t_map *map);
+char	*dup_without_newline(char *line);
+int		parse_map_line(char *line, t_map *map);
+int		get_max_width(t_list *raw_map);
+char	*pad_line(char *raw_line, int max_width);
+int		build_final_map(t_map *map);
+int		is_floor_or_player(char c);
+int		check_neighbors(t_map *map, int y, int x);
+int		validate_map_walls(t_map *map);
+void	print_map_struct(t_map *map, char *step);
+void	print_map_grid(t_map *map);
 
 #endif
